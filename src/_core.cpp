c1a974ac1d89be159b42d1cd7b44af17641ed819
JI QI <jq7140@princeton.edu>
2023-12-12 22:08:50 -0500

    test 4

--- src/_core.cpp
+++ src/_core.cpp
@@ -8,7 +8,5 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(_core, m) {
   /// ... Fill me in!
-  py::class_<HarmonicOscillator>(m, "HarmonicOscillator")
-      .def(py::init<>())
-
+  py::class_<HarmonicOscillator>(m, "HarmonicOscillator").def(py::init<>())
 }
