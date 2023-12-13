b0e957c223720ec0153f03c0bf2baf9925dd7816
JI QI <jq7140@princeton.edu>
2023-12-12 22:20:37 -0500

    test 5

--- src/_core.cpp
+++ src/_core.cpp
@@ -7,6 +7,16 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(_core, m) {
-  /// ... Fill me in!
-  py::class_<HarmonicOscillator>(m, "HarmonicOscillator").def(py::init<>())
+    py::class_<IntegratorBase, std::shared_ptr<IntegratorBase>>(m, "IntegratorBase")
+        .def(py::init<>())
+        .def("compute_step", &IntegratorBase::compute_step)
+        .def("integrate", &IntegratorBase::integrate);
+
+    py::class_<EulerIntegrator, IntegratorBase, std::shared_ptr<EulerIntegrator>>(m, "EulerIntegrator")
+        .def(py::init<>())
+        .def("compute_step", &EulerIntegrator::compute_step);
+
+    py::class_<HarmonicOscillator, EulerIntegrator, std::shared_ptr<HarmonicOscillator>>(m, "HarmonicOscillator")
+        .def(py::init<>())
+        .def("f", &HarmonicOscillator::f);
 }
